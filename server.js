d09f25a2ad45230f3478d998ca07e9778df68dfd
onser23 <semed.semedzade@yahoo.com>
2026-04-13 16:47:51 +0400

    new

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -26,8 +25,6 @@ app.use(function (req, res, next) {
   );
   next();
 });
-
-// app.options("*", cors());
 
 // Routes
 app.use("/api/auth", authRoutes);
