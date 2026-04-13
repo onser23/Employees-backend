35d5ec239915f2128fb06af3772a23405ee5a780
onser23 <semed.semedzade@yahoo.com>
2026-04-13 16:38:40 +0400

    cors

--- server.js
+++ server.js
@@ -17,14 +17,17 @@ connectDB();
 const app = express();
 
 // Middleware
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "https://employees-frontend-ecru.vercel.app",
-  }),
-);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
 
 app.options("*", cors());
 
